d5ac97c31629102d90bc9fec15c04e37bb95301b
hrant <hrant@egern.net>
2014-11-25 01:52:33 +0400

    SectionHarvester.js - get the list of pages

--- SectionHarvester.js
+++ SectionHarvester.js
@@ -1,5 +1,9 @@
 $(function () {
 	"use strict";
+	var proofreadNamespaces = {
+		index: 106,
+		page: 104
+	};
 	var $body = $('#mw-content-text');
 	var $input = $('<input type="text">');
 	var $button = $('<button disabled="disabled">');
@@ -9,17 +13,17 @@ $(function () {
 		$body.text('');
 
 		$input.appendTo($body);
-		$input.attr('placeholder', 'URL of the book');
+		$input.attr('placeholder', 'URL of the index page');
 		$input.on('change', validateInput);
 		$input.on('keyup', validateInput);
 
 		$button.appendTo($body);
 		$button.text('Harvest!');
 
-		$button.click();
+		$button.click(getPages);
 	};
 	var validateURL = function (url) {
-		// TODO: validate!
+		// TODO: validate! - only current wikisource!
 		return url.match(/^https?:\/\/[a-z-]+.wikisource.org\/wiki\/.+/) !== null;
 	};
 	var validateInput = function () {
@@ -31,7 +35,58 @@ $(function () {
 			$button.attr('disabled', 'disabled');
 		}
 	};
+	var updateProofreadNamespaces = function () {
+		// https://www.mediawiki.org/wiki/Extension_talk:Proofread_Page#API_Documentation_.26_Improvement
+		var deferred = $.Deferred();
+		$.get('/w/api.php', {
+			"action": "query",
+			"meta": "proofreadinfo",
+			"format": "json",
+			"piprop": "namespaces"
+		}).success(function (d) {
+			proofreadNamespaces = d.query.proofreadnamespaces;
+			deferred.resolve();
+		});
+		return deferred;
+	};
+	var getPages = function () {
+		var urlArray = $input.val().substr(6).split(':'); // find namespace colon
+		var url = decodeURIComponent(urlArray[1]);
+		getPagesRecursive(url).then(function (pages) {
+			pages.map(function (page) {
+				// TODO: temporaryily print the page titles
+				$body.append('<p>#' + page.pageid + ' - ' + page.title + '</p>');
+			});
+		});
+	};
+	var getPagesRecursive = function (url, queryContinue) {
+		var deferred = $.Deferred();
+		var getObject = {
+			"action": "query",
+			"list": "allpages",
+			"aplimit": "500",
+			"format": "json",
+			"apprefix": url,
+			"apnamespace": proofreadNamespaces.page.id
+		};
+		getObject = $.extend(getObject, queryContinue || {});
+		$.get('/w/api.php', getObject).success(function (d) {
+			if (d["query-continue"]) {
+				// not all results have been received
+				// add the value of .query-continue.allpages to the query next time
+				getPagesRecursive(url, d["query-continue"].allpages).then(function (allpages) {
+					deferred.resolve(d.query.allpages.concat(allpages));
+				});
+			} else {
+				deferred.resolve(d.query.allpages);
+			}
+		});
+		return deferred;
+	};
 
-
-	initUI();
+	// get information about namespaces that Proofread extension uses
+	updateProofreadNamespaces().then(initUI).fail(function () {
+		// TODO:
+		alert('Cannot use Wikisource API');
+	});
 });
