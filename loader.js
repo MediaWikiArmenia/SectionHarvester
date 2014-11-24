b9b35bc2ede1fd6b825b4e3e4e78f8129c105ab7
hrant <hrant@egern.net>
2014-11-24 16:21:19 +0400

    loader.js - the user script loader

---
+++ loader.js
@@ -0,0 +1,19 @@
+(function loadSectionHarvester() {
+	if (mw.config.get('wgNamespace') !== -1) {
+		// SpecialPage namespace, language independent
+		return;
+	}
+	var pageName = mw.config.get('wgPageName').split(':');
+	if (pageName.length < 1) {
+		// No page specified
+		return;
+	}
+	if (pageName[1] !== 'SectionHarvester') {
+		// Different Special Page
+		return;
+	}
+
+	// TODO: where we should put this JS so it is accessible from all language editions
+	importScript('User:HrantKhachatrian/SectionHarvester.js');
+
+})();
