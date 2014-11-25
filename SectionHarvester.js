7e8e3ed70db8f09ed7b3933a7e41c87a668e077f
hrant <hrant@egern.net>
2014-11-25 20:36:21 +0400

    SectionHarvester.js - refactoring (UI object) + get page contents + simple section harvester

--- SectionHarvester.js
+++ SectionHarvester.js
@@ -4,36 +4,49 @@ $(function () {
 		index: 106,
 		page: 104
 	};
-	var $body = $('#mw-content-text');
-	var $input = $('<input type="text">');
-	var $button = $('<button disabled="disabled">');
-	var initUI = function () {
-		// TODO: i18n
-		$('#firstHeading span').text('SectionHarvester');
-		$body.text('');
+	var fetchedPages = {};
+	var foundSections = {};
 
-		$input.appendTo($body);
-		$input.attr('placeholder', 'URL of the index page');
-		$input.on('change', validateInput);
-		$input.on('keyup', validateInput);
+	var UI = {
+		$body: $('#mw-content-text'),
+		$input: $('<input type="text">'),
+		$button: $('<button disabled="disabled">'),
+		$status: $('<div style="background:#EEE;padding:5px 10px;text-align: center">'),
+		init: function () {
+			// TODO: i18n
+			$('#firstHeading span').text('SectionHarvester');
+			UI.$body.text('');
+			UI.$status.appendTo(UI.$body);
 
-		$button.appendTo($body);
-		$button.text('Harvest!');
+			UI.$input.appendTo(UI.$body);
+			UI.$input.attr('placeholder', 'URL of the index page');
+			UI.$input.on('change', UI.validateInput);
+			UI.$input.on('keyup', UI.validateInput);
 
-		$button.click(getPages);
+			UI.$button.appendTo(UI.$body);
+			UI.$button.text('Harvest!');
+
+			UI.$button.click(harvestSections);
+		},
+		validateInput: function () {
+			var url = UI.$input.val();
+			// TODO: some error message?
+			if (validateURL(url)) {
+				UI.$button.removeAttr('disabled');
+			} else {
+				UI.$button.attr('disabled', 'disabled');
+			}
+		},
+		getInputValue: function () {
+			return UI.$input.val();
+		},
+		setStatus: function (message) {
+			UI.$status.text(message);
+		}
 	};
 	var validateURL = function (url) {
 		// TODO: validate! - only current wikisource!
 		return url.match(/^https?:\/\/[a-z-]+.wikisource.org\/wiki\/.+/) !== null;
-	};
-	var validateInput = function () {
-		var url = $input.val();
-		// TODO: some error message?
-		if (validateURL(url)) {
-			$button.removeAttr('disabled');
-		} else {
-			$button.attr('disabled', 'disabled');
-		}
 	};
 	var updateProofreadNamespaces = function () {
 		// https://www.mediawiki.org/wiki/Extension_talk:Proofread_Page#API_Documentation_.26_Improvement
@@ -49,15 +62,57 @@ $(function () {
 		});
 		return deferred;
 	};
-	var getPages = function () {
-		var urlArray = $input.val().substr(6).split(':'); // find namespace colon
+	var harvestSections = function () {
+		var urlArray = UI.getInputValue().substr(6).split(':'); // find namespace colon
 		var url = decodeURIComponent(urlArray[1]);
+		UI.setStatus('Fetching list of pages...'); //TODO: i18n
 		getPagesRecursive(url).then(function (pages) {
 			pages.map(function (page) {
-				// TODO: temporaryily print the page titles
-				$body.append('<p>#' + page.pageid + ' - ' + page.title + '</p>');
+				// TODO: temporarily print the page titles
+				//	UI.$body.append('<p>#' + page.pageid + ' - ' + page.title + '</p>');
 			});
+			UI.setStatus('Fetching page contents...');
+			var pageids = pages.map(function (p) {
+				return p.pageid;
+			}).join('|');
+			return getPageContentsRecursive(pageids);
+		}).then(function (pages) {
+			fetchedPages = pages;
+			Object.keys(fetchedPages).map(scanPage);
+			return true;
+		}).then(function () {
+			console.log(foundSections);
 		});
+	};
+	var scanPage = function (id) {
+		UI.setStatus('Scanning page:' + fetchedPages[id].title);
+		var content = fetchedPages[id].revisions[0]['*'];
+		var offset = 0;
+		fetchedPages[id].foundSections = -1;
+		do {
+			fetchedPages[id].foundSections++;
+			offset = findNextSection(id, content, offset);
+		} while (offset !== false);
+	};
+	var findNextSection = function (pageid, content, offset) {
+		var sectionBeginString = '<section begin="'; // TODO: i18n ? https://www.mediawiki.org/wiki/Extension:Labeled_Section_Transclusion#Localisation
+		var sectionEndString = '<section end="';
+
+		var beginTagStart = content.indexOf(sectionBeginString, offset);
+		if (beginTagStart === -1) {
+			return false;
+		}
+		var beginNameStart = beginTagStart + sectionBeginString.length;
+		var beginNameEnd = content.indexOf('"', beginNameStart); // TODO: quotes in the title?
+		var beginTagEnd = content.indexOf('>', beginNameEnd);
+		var sectionName = content.substring(beginNameStart, beginNameEnd);
+		if (!foundSections[sectionName]) {
+			foundSections[sectionName] = [];
+		}
+		foundSections[sectionName].push(pageid);
+		var endTagStart = content.indexOf(sectionEndString, beginTagEnd);
+		// TODO: ...
+		return offset;
 	};
 	var getPagesRecursive = function (url, queryContinue) {
 		var deferred = $.Deferred();
@@ -84,8 +139,33 @@ $(function () {
 		return deferred;
 	};
 
+	var getPageContentsRecursive = function (pageids, queryContinue) {
+		var deferred = $.Deferred();
+		var getObject = {
+			"action": "query",
+			"prop": "revisions",
+			"rvprop": "content",
+			"format": "json",
+			"pageids": pageids
+		};
+		getObject = $.extend(getObject, queryContinue || {});
+		$.get('/w/api.php', getObject).success(function (d) {
+			if (d["query-continue"]) {
+				// not all results have been received
+				// add the value of .query-continue.allpages to the query next time
+				getPageContentsRecursive(pageids, d["query-continue"].revisions).then(function (pages) {
+					deferred.resolve(d.query.pages.concat(pages));
+				});
+			} else {
+				deferred.resolve(d.query.pages);
+			}
+		});
+		return deferred;
+
+	};
+
 	// get information about namespaces that Proofread extension uses
-	updateProofreadNamespaces().then(initUI).fail(function () {
+	updateProofreadNamespaces().then(UI.init).fail(function () {
 		// TODO:
 		alert('Cannot use Wikisource API');
 	});
